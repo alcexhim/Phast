844722c34f2e113803a8a66b2d8b04da8e72c54e
alcexhim <alcexhim@gmail.com>
2015-03-09 10:33:33 -0400

    Made WebControlClientIDMode an actual "Enumeration" class

--- ServerSide/WebControlClientIDMode.inc.php
+++ ServerSide/WebControlClientIDMode.inc.php
@@ -1,3 +1,22 @@
 <?php
-	\Enum::Create("Phast\\WebControlClientIDMode", "None", "Automatic", "Manual");
+	namespace Phast;
+	
+	abstract class WebControlClientIDMode extends Enumeration
+	{
+		/**
+		 * ClientIDs are not assigned or the implementation is unspecified.
+		 * @var WebControlClientIDMode
+		 */
+		const None = 0;
+		/**
+		 * ClientIDs are automatically assigned by PHAST.
+		 * @var WebControlClientIDMode
+		 */
+		const Automatic = 1;
+		/**
+		 * ClientIDs are manually specified by the Web site author.
+		 * @var WebControlClientIDMode
+		 */
+		const Manual = 2;
+	}
 ?>
