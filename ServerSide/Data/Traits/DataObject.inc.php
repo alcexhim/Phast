5265e19a9297808bf9149a0e5b1a58e818e34709
alcexhim <alcexhim@gmail.com>
2015-03-11 14:24:49 -0400

    Implemented object-agnostic GetByAssoc() function, refactoring the logic in Get()

--- ServerSide/Data/Traits/DataObject.inc.php
+++ ServerSide/Data/Traits/DataObject.inc.php
@@ -9,10 +9,39 @@
 	{
 		public static $DataObjectTableName;
 		public static $DataObjectColumnPrefix;
-		
+
+		/**
+		 * Called when a property on the DataObject is being bound to a data column.
+		 * @param string $columnName The name of the column to bind.
+		 * @param mixed $value The value to assign to the column.
+		 * @return boolean True if the data binding was handled by this function; false otherwise.
+		 */
 		protected function BindDataColumn($columnName, $value)
 		{
-			return true;
+			return false;
+		}
+		
+		public static function GetByAssoc($values)
+		{
+			$columnNames = array_keys($values);
+			$columnPrefix = self::$DataObjectColumnPrefix;
+			$columnCount = count($columnNames);
+			
+			$item = new self();
+			for ($j = 0; $j < $columnCount; $j++)
+			{
+				$columnName = $columnNames[$j];
+				$realColumnName = $columnName;
+				if (stripos($realColumnName, $columnPrefix) === 0)
+				{
+					$realColumnName = substr($realColumnName, strlen($columnPrefix));
+				}
+				if (!$item->BindDataColumn($realColumnName, $values[$columnName]))
+				{
+					$item->{$realColumnName} = $values[$columnName];
+				}
+			}
+			return $item;
 		}
 		
 		/**
@@ -27,32 +56,10 @@
 			$count = $statement->rowCount();
 			$retval = array();
 			
-			$columnNames = array();
-			$columnCount = $statement->columnCount();
-			for ($c = 0; $c < $columnCount; $c++)
-			{
-				$meta = $statement->getColumnMeta($c);
-				$columnNames[] = $meta["name"];
-			}
-
-			$columnPrefix = self::$DataObjectColumnPrefix;
 			for ($i = 0; $i < $count; $i++)
 			{
 				$values = $statement->fetch(PDO::FETCH_ASSOC);
-				$item = new self();
-				for ($j = 0; $j < $columnCount; $j++)
-				{
-					$columnName = $columnNames[$j];
-					$realColumnName = $columnName;
-					if (stripos($realColumnName, $columnPrefix) === 0)
-					{
-						$realColumnName = substr($realColumnName, strlen($columnPrefix)); 
-					}
-					if ($item->BindDataColumn($realColumnName, $values[$columnName]))
-					{
-						$item->{$realColumnName} = $values[$columnName];
-					}
-				}
+				$item = self::GetByAssoc($values);
 				$retval[] = $item;
 			}
 			return $retval;
