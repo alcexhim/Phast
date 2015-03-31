fb542e19c8ff3bba949cf9f5e9cd12a6ff489665
alcexhim <alcexhim@gmail.com>
2015-03-30 23:41:56 -0400

    Added Timeline control (work in progress)

---
+++ ServerSide/WebControls/Timeline.inc.php
@@ -0,0 +1,56 @@
+<?php
+	namespace Phast\WebControls;
+	
+	use Phast\WebControl;
+	use Phast\HTMLControl;
+	
+	class Timeline extends WebControl
+	{
+		public $Posts;
+		
+		public function __construct()
+		{
+			$this->ParseChildElements = true;
+			$this->ClassList[] = "Timeline";
+			$this->TagName = "div";
+		}
+		
+		protected function RenderBeginTag()
+		{
+			$this->Controls = array();
+			foreach ($this->Posts as $post)
+			{
+				$divPost = new HTMLControl("div");
+				$divPost->ClassList[] = "TimelinePost";
+				
+				$divPostHeader = new HTMLControl("div");
+				$divPostHeader->ClassList[] = "Header";
+				$divPost->Controls[] = $divPostHeader;
+				
+				$divPostContent = new HTMLControl("div");
+				$divPostContent->ClassList[] = "Content";
+				$divPost->Controls[] = $divPostContent;
+				
+				$divPostFooter = new HTMLControl("div");
+				$divPostFooter->ClassList[] = "Footer";
+				$divPost->Controls[] = $divPostFooter;
+				
+				$this->Controls[] = $divPost;
+			}
+			parent::RenderBeginTag();
+		}
+	}
+	class TimelinePost
+	{
+		public $ID;
+		public $CreationUserName;
+		public $CreationUserImageURL;
+		public $Controls;
+		public $ViewCount;
+		
+		public function __construct()
+		{
+			$this->ParseChildElements = true;
+		}
+	}
+?>
