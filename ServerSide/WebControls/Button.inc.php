8b0d9f72fcf391f0e5da9d19f1175ddefaa7455b
alcexhim <alcexhim@gmail.com>
2015-04-01 16:35:28 -0400

    Better Button server-side control, including support for FA icons

--- ServerSide/WebControls/Button.inc.php
+++ ServerSide/WebControls/Button.inc.php
@@ -7,9 +7,14 @@
 	use Phast\WebControlAttribute;
 	
 	use Phast\HTMLControl;
-			
+use Phast\Phast;
+				
 	class Button extends WebControl
 	{
+		public $TargetFrame;
+		public $TargetURL;
+		public $TargetScript;
+		
 		public $Text;
 		
 		protected function RenderContent()
@@ -35,10 +40,42 @@
 				$tag->ClassList = $this->ClassList;
 				$tag->ClassList[] = "Button";
 				$tag->ClassList[] = $this->CssClass;
-				$tag->Attributes[] = new WebControlAttribute("id", $this->ClientID);
-				$tag->Attributes[] = new WebControlAttribute("href", "#");
+				if ($this->ClientID != null)
+				{
+					$tag->Attributes[] = new WebControlAttribute("id", $this->ClientID);
+				}
+				else
+				{
+					$tag->Attributes[] = new WebControlAttribute("id", $this->ID);
+				}
 				
-				$tag->InnerHTML = $this->Text;
+				if ($this->TargetFrame != null)
+				{
+					$tag->Attributes[] = new WebControlAttribute("target", $this->TargetFrame);
+				}
+				if ($this->TargetURL != null)
+				{
+					$tag->Attributes[] = new WebControlAttribute("href", $this->TargetURL);
+				}
+				if ($this->TargetScript != null)
+				{
+					$tag->Attributes[] = new WebControlAttribute("href", "#");
+					$this->Attributes[] = new WebControlAttribute("onclick", $this->TargetScript);
+				}
+				
+				if ($this->IconName != null)
+				{
+					$i = new HTMLControl("i");
+					$i->ClassList[] = "fa";
+					$i->ClassList[] = "fa-" . $this->IconName;
+					$tag->Controls[] = $i;
+				}
+				
+				$spanText = new HTMLControl("span");
+				$spanText->ClassList[] = "Text";
+				$spanText->InnerHTML = $this->Text;
+				
+				$tag->Controls[] = $spanText;
 				
 				$tag->Render();
 			}
