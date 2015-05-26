f7684f46581b97f2906f6a4f95666bc1a6bde47f
alcexhim <alcexhim@gmail.com>
2015-05-26 10:29:55 -0400

    Implement 304 Not Modified for System.js.php

--- ClientSide/Scripts/System.js.php
+++ ClientSide/Scripts/System.js.php
@@ -1,5 +1,20 @@
 <?php
 	include("jsmin.php");
+	
+	$last_modified_time = filemtime(__FILE__);
+	$etag = md5_file(__FILE__);
+	// always send headers
+	header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
+	header("Etag: $etag");
+	
+	// exit if not modified
+	if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time ||
+		@trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag)
+	{
+		header("HTTP/1.1 304 Not Modified");
+		exit;
+	}
+	
 	header ("Content-Type: text/javascript");
 	
 	$bundles = array();
