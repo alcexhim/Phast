c6fe4d97b4580157e1e22341a9839da323501b29
alcexhim <alcexhim@gmail.com>
2015-06-02 16:40:11 -0400

    Added ToggleSwitch control

---
+++ ClientSide/Scripts/Controls/ToggleSwitch.js
@@ -0,0 +1,52 @@
+function ToggleSwitch(parentElement)
+{
+	this.ParentElement = parentElement;
+	parentElement.addEventListener("click", function(e)
+	{
+		var changed = System.ClassList.Toggle(this, "Checked");
+		if (changed) this.NativeObject.on_Changed();
+
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	});
+
+	this.EventHandlers =
+	{
+		"Changed": new System.EventHandler()
+	};
+
+	this.on_Changed = function()
+	{
+		this.EventHandlers.Changed.Execute(this, null);
+	};
+
+	this.set_Checked = function(value)
+	{
+		if (this.get_Checked() == value) return false;
+
+		if (value)
+		{
+			System.ClassList.Add(this.ParentElement, "Checked");
+		}
+		else
+		{
+			System.ClassList.Remove(this.ParentElement, "Checked");
+		}
+		this.on_Changed();
+		return true;
+	};
+	this.get_Checked = function()
+	{
+		return System.ClassList.Contains(this.ParentElement, "Checked");
+	};
+}
+
+window.addEventListener("load", function ()
+{
+	var items = document.getElementsByClassName("ToggleSwitch");
+	for (var i = 0; i < items.length; i++)
+	{
+		items[i].NativeObject = new ToggleSwitch(items[i]);
+	}
+});
